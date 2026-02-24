77b3be76f927d21a924d6caea539cc3ba1925794
sharu1902 <chsharanya2002@gmail.com>
2026-02-24 07:46:37 +0530

    Add backend Dockerfile

--- crud-dd-task-mean-app/backend/app/models/index.js
+++ crud-dd-task-mean-app/backend/app/models/index.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = process.env.MONGO_URI || "mongodb://localhost:27017/testdb";
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
